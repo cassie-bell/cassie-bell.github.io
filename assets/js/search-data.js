3e9c3d0d620980760e8732f68179426080021083
cassie-bell <cassieannbell@gmail.com>
2026-05-07 18:34:18 +0000

    Deploying to gh-pages from @ cassie-bell/cassie-bell.github.io@3c2ec570cc103dd555b2d568b60fecc8b25049dd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -479,6 +479,13 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/cassandra-bell-ru27", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
