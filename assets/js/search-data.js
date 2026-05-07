f9e8f86e381f2d91153d7575274fc559751425b2
cassie-bell <cassieannbell@gmail.com>
2026-05-07 21:32:16 +0000

    Deploying to gh-pages from @ cassie-bell/cassie-bell.github.io@0b861a14b89271bd25d9dcd3550b3b1f51f5815b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "What I am up to",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
